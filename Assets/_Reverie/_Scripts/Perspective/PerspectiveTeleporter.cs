f701e0862198b02e7016a64da90f546c96dadebf
YoshuGH <axeleonardo2000@gmail.com>
2022-01-27 23:28:24 -0600

    Perspective Mechanic Done

---
+++ Assets/_Reverie/_Scripts/Perspective/PerspectiveTeleporter.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PerspectiveTeleporter : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform teleporterReceiver;
+
+    private bool playerIsOverlapping = false;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        if(playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = playerTransform.position - transform.position;
+            float dot = Vector3.Dot(portalToPlayer, transform.forward);
+
+            //The player has moved across the portal
+            if (dot < 0f)
+            {
+                //Teleport Stuff
+                float rotationDiff = -Quaternion.Angle(transform.rotation, teleporterReceiver.rotation);
+                rotationDiff += 180;
+                playerTransform.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 posOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                playerTransform.position = teleporterReceiver.position + posOffset;
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            playerIsOverlapping = true; print("Entre");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerIsOverlapping = false; print("Sali");
+    }
+}
